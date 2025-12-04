8bc8b85be16d07115e29327872ce83a7351d1725
Mateo Del Lungo <412337@tecnicatura.frc.utn.edu.ar>
2025-12-04 02:28:20 -0300

    Creado controller de prueba

---
+++ Backend/src/chat-in-realtime/Controllers/TestController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace chat_in_realtime.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                Status = "Online",
+                Message = "La API está levantada y corriendo 🚀",
+                ServerTime = DateTime.Now
+            });
+        }
+    }
+}
--- Backend/src/chat-in-realtime/obj/Debug/net8.0/chat-in-realtime.AssemblyInfo.cs
+++ Backend/src/chat-in-realtime/obj/Debug/net8.0/chat-in-realtime.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("chat-in-realtime")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5f6e4c5488f114154bdd0f49683a8bddc321c2d")]
 [assembly: System.Reflection.AssemblyProductAttribute("chat-in-realtime")]
 [assembly: System.Reflection.AssemblyTitleAttribute("chat-in-realtime")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
