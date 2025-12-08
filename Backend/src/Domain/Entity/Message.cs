9c8787aab2a75441502ad19524eb30b9f2bce0a4
Tomas Aparicio <96070@sistemas.frc.utn.edu.ar>
2025-12-08 02:46:56 -0300

    entidades

---
+++ Backend/src/Domain/Entity/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entity
+{
+    internal class Message
+    {
+        private Guid Id { get; set; }
+        private User Sender { get; set; }
+        private string Content { get; set; }
+        private DateTime Timestamp { get; set; }
+
+        //private int receiverId; maybe? al pedo
+    }
+}
---
+++ Backend/src/Domain/Entity/Picture.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entity
+{
+    internal class Picture
+    {
+        private Guid Id { get; set; }
+        private byte[] Data { get; set; }
+        // No se si esta bien el tipo de dato
+        // Debe ser un blob en la base de datos
+    }
+}
