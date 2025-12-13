f373319565b3a0f851033fe58ae5be575d66fe2b
Mateo Del Lungo <412337@tecnicatura.frc.utn.edu.ar>
2025-12-12 21:00:43 -0300

    Removed unnecessary "using" statements
    Co-Authored-By: Mateo Del Lungo <152237159+Mudo0@users.noreply.github.com>

--- Backend/src/Domain/Entities/Picture.cs
+++ Backend/src/Domain/Entities/Picture.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Domain.Entity
+﻿namespace Domain.Entity
 {
-    internal class Picture
+    public class Picture
     {
         private Guid Id { get; set; }
         private byte[] Data { get; set; }
