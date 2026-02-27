16103c2101ed88bc777bca4c854c296c93cc7bbb
Mateo Del Lungo <412337@tecnicatura.frc.utn.edu.ar>
2026-02-27 19:48:38 -0300

    refactor:  implement dtos in SendMessage()
    Co-Authored-By: Mateo Del Lungo <152237159+Mudo0@users.noreply.github.com>

--- Backend/src/chat-in-realtime/Hubs/ChatHub.cs
+++ Backend/src/chat-in-realtime/Hubs/ChatHub.cs
@@ -1,26 +1,66 @@
+using Application.Common;
 using Application.Interfaces;
+using Domain.Entities;
 using Microsoft.AspNetCore.SignalR;
+using System.Collections.Concurrent;
 
 namespace chat_in_realtime.Hubs;
 
 public class ChatHub : Hub
 {
-    private static readonly List<object> _messages = [];
     private readonly IMessageService _messageService;
+    private readonly IUserService _userService;
 
-    public ChatHub(IMessageService messageService)
+    public ChatHub(IMessageService messageService, IUserService userService)
     {
         _messageService = messageService;
+        _userService = userService;
     }
 
-    public async Task SendMessage(string username, string content)
+    public async Task SendMessage(SendMessageDTO messageIn)
     {
-        _messages.Add(new { username, content });
-        await Clients.All.SendAsync("ReceiveMessage", username, content);
+        //identificar el usuario
+        // NECESITA JWT
+        string userIdString = Context.UserIdentifier ?? throw new HubException("No autorizado");
+        Guid userId = Guid.Parse(userIdString);
+
+        //buscarlo en la bd
+        //se puede mejorar el manejo de erorres
+        var user = await _userService.GetUserByIdAsync(userId);
+        if (user == null)
+        {
+            throw new HubException("Usuario no encontrado.");
+        }
+
+        //entidad para la bd
+        var newMessage = new Message(
+            id: Guid.NewGuid(),
+            sender: user,
+            content: messageIn.Content,
+            timestamp: DateTime.UtcNow
+            );
+
+        //guardar en bd
+        await _messageService.SaveMessageAsync(newMessage);
+
+        //dto para el hub
+        var messageToBroadcast = new MessageReceivedDTO(
+            Id: newMessage.Id,
+            Content: newMessage.Content,
+            Timestamp: newMessage.Timestamp,
+            Sender: new UserSenderDTO
+            (
+                Id: user.Id,
+                Username: user.Username
+            )
+
+        );
+        //mensaje enviado al hub
+        await Clients.All.SendAsync("ReceiveMessage", messageToBroadcast);
     }
 
     public async Task LoadMessages()
     {
-        await Clients.Caller.SendAsync("LoadMessages", _messages);
+        throw new NotImplementedException();
     }
 }
