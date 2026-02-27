1606c4edf606b859d71be2f75a1ae8b03bcbf27e
Mateo Del Lungo <412337@tecnicatura.frc.utn.edu.ar>
2026-02-27 19:12:35 -0300

    feat: add UserRepository
    Co-Authored-By: Mateo Del Lungo <152237159+Mudo0@users.noreply.github.com>

---
+++ Backend/src/Application/Interfaces/IUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities;
+
+namespace Application.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<User> GetByIdAsync(Guid id);
+
+        Task AddAsync(User user);
+    }
+}
---
+++ Backend/src/Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,19 @@
+﻿
+using Application.Interfaces;
+using Domain.Entities;
+
+namespace Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        public Task AddAsync(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
