fa29e313ce01a1a16dd922b1257674345838ca75
Mateo Del Lungo <412337@tecnicatura.frc.utn.edu.ar>
2026-02-27 19:12:21 -0300

    feat: add UserService
    Co-Authored-By: Mateo Del Lungo <152237159+Mudo0@users.noreply.github.com>

---
+++ Backend/src/Application/Services/UserService.cs
@@ -0,0 +1,13 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+
+namespace Application.Services
+{
+    public class UserService : IUserService
+    {
+        public Task<User> GetUserByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
