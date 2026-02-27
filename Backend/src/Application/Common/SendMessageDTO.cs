17f3e67c1680f6a71fedb243cd65d2d58e06aa96
Mateo Del Lungo <412337@tecnicatura.frc.utn.edu.ar>
2026-02-27 18:10:58 -0300

    feat: add SentMessageDTO

---
+++ Backend/src/Application/Common/SendMessageDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.Common
+{
+    /// <summary>
+    /// Mensaje de entrada desde el cliente al servidor.
+    /// </summary>
+    public sealed record class SendMessageDTO(string Content);
+}
