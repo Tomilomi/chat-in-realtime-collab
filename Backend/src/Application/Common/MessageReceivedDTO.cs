f3327d57205ad1cf4003c0a6e09ae4d1c8b06619
Mateo Del Lungo <412337@tecnicatura.frc.utn.edu.ar>
2026-02-27 18:23:16 -0300

    feat: add  MessageReceivedDTO
    Co-Authored-By: Mateo Del Lungo <152237159+Mudo0@users.noreply.github.com>

---
+++ Backend/src/Application/Common/MessageReceivedDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Common
+{
+    /// <summary>
+    /// Mensaje de salida desde el servidor al cliente, representando un mensaje recibido en el chat.
+    /// </summary>
+
+    public sealed record MessageReceivedDTO(Guid Id,
+        string Content, DateTime Timestamp, string SenderUsername);
+}
