35561842a7b555471afae460d6ad9aa8b6af117e
Tomilomi <tomasmgoyenecheap@gmail.com>
2026-03-02 20:45:40 -0300

    adding jwt

--- Backend/src/chat-in-realtime/Program.cs
+++ Backend/src/chat-in-realtime/Program.cs
@@ -6,8 +6,45 @@ using Microsoft.EntityFrameworkCore;
 using chat_in_realtime.Hubs;
 using Domain.Entities;
 using chat_in_realtime.Handlers;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// JWT
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!))
+        };
+
+        // esto es para que SignalR pueda usar JWT
+        options.Events = new JwtBearerEvents
+        {
+            OnMessageReceived = context =>
+            {
+                var accessToken = context.Request.Query["access_token"];
+                var path = context.HttpContext.Request.Path;
+                if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/chathub"))
+                {
+                    context.Token = accessToken;
+                }
+                return Task.CompletedTask;
+            }
+        };
+    });
+
+
 
 // CORS
 builder.Services.AddCors(options =>
@@ -54,6 +91,11 @@ if (app.Environment.IsDevelopment())
 //app.UseHttpsRedirection();
 
 app.UseCors();
+
+
+// JWT
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
