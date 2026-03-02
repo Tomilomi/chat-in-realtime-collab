ebde06e4c96f5265c6666299230c4b1e09c67deb
Mateo Del Lungo <412337@tecnicatura.frc.utn.edu.ar>
2026-03-02 20:08:02 -0300

    feat: add GlobalExceptionHandler
    Co-Authored-By: Mateo Del Lungo <152237159+Mudo0@users.noreply.github.com>

---
+++ Backend/src/chat-in-realtime/Handlers/GlobalExceptionHandler.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace chat_in_realtime.Handlers
+{
+    /// <summary>
+    /// Clase que captura excepciones a nivel global.
+    /// </summary>
+    public class GlobalExceptionHandler : IExceptionHandler
+    {
+        private readonly ILogger<GlobalExceptionHandler> _logger;
+
+        public GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger)
+        {
+            _logger = logger;
+        }
+
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext,
+            Exception exception, CancellationToken cancellationToken)
+        {
+            _logger.LogError(exception, "Ocurrió una excepción inesperada en la app: {Message}", exception.Message);
+
+            //respuesta estandar
+            var problemDetails = new ProblemDetails
+            {
+                Status = StatusCodes.Status500InternalServerError,
+                Title = "Error Interno del Servidor",
+                Detail = "Ocurrió un error inesperado. Por favor, inténtelo de nuevo más tarde."
+            };
+
+            httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            await httpContext.Response.WriteAsJsonAsync(problemDetails, cancellationToken);
+
+            return true;
+        }
+    }
+}
