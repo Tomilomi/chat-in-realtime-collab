46b7369611eb55a5c73c6aefb48e5f5ee441d3ad
Mateo Del Lungo <412337@tecnicatura.frc.utn.edu.ar>
2026-03-02 20:40:37 -0300

    feat: add extension method for DependencyInjection
    Co-Authored-By: Mateo Del Lungo <152237159+Mudo0@users.noreply.github.com>

---
+++ Backend/src/Application/Extensions/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Application.Interfaces;
+using Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Extensions
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddScoped<IMessageService, MessageService>();
+            services.AddScoped<IUserService, UserService>();
+
+            return services;
+        }
+    }
+}
---
+++ Backend/src/chat-in-realtime/Extensions/DependencyInjection.cs
@@ -0,0 +1,36 @@
+﻿using chat_in_realtime.Handlers;
+
+namespace chat_in_realtime.Extensions
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPresentation(this IServiceCollection services)
+        {
+            // CORS
+            services.AddCors(options =>
+             {
+                 options.AddDefaultPolicy(policy =>
+                 {
+                     policy.SetIsOriginAllowed(_ => true)
+                         .AllowAnyHeader()
+                         .AllowAnyMethod()
+                         .AllowCredentials();
+                 });
+             });
+
+            //controllers y swagger
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            // SignalR
+            services.AddSignalR();
+
+            // Manejo global de excepciones
+            services.AddExceptionHandler<GlobalExceptionHandler>();
+            services.AddProblemDetails();
+
+            return services;
+        }
+    }
+}
