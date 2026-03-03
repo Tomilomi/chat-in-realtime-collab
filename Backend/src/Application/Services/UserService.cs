8f834ac232a04b0aeb7d57ffd45820189169283e
Mateo Del Lungo <412337@tecnicatura.frc.utn.edu.ar>
2026-03-02 22:54:48 -0300

    refactor: implement errorOr in UserService
    Co-Authored-By: Mateo Del Lungo <152237159+Mudo0@users.noreply.github.com>

--- Backend/src/Application/Services/UserService.cs
+++ Backend/src/Application/Services/UserService.cs
@@ -1,5 +1,7 @@
 ﻿using Application.Interfaces;
 using Domain.Entities;
+using Domain.Errors;
+using ErrorOr;
 
 namespace Application.Services
 {
@@ -13,32 +15,28 @@ namespace Application.Services
             _userRepository = userRepository;
             _pictureRepository = pictureRepository;
         }
-        
-        
-        public async Task<User?> GetUserByIdAsync(Guid id)
+
+        public async Task<ErrorOr<User>> GetUserByIdAsync(Guid id)
         {
-            return await _userRepository.GetByIdAsync(id);
+            var result = await _userRepository.GetByIdAsync(id);
+            if (result == null) { return DomainErrors.User.NotFound; }
+            return result;
         }
-        
 
         public async Task RegisterAsync(string userName, string password)
         {
-            
             var picture = await _pictureRepository.GetDefaultAsync();
-            
+
             var user = User.Create(userName, password, picture!);
             if (user.IsError) return;
             await _userRepository.AddAsync(user.Value);
         }
 
-        public async Task<User?> LoginAsync(string userName, string password)
+        public async Task<ErrorOr<User>> LoginAsync(string userName, string password)
         {
             var user = await _userRepository.GetByUserNameAsync(userName);
-            if (user is null) return null;
-            return user.Password != password ? null : user;
+            if (user is null) return DomainErrors.User.NotFound;
+            return user.Password != password ? DomainErrors.User.InvalidPassword : user;
         }
-        
-        
-        
     }
 }
