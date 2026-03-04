af1ec1caec69f03f2e7f8ea38b75db9b5150339a
Mateo Del Lungo <412337@tecnicatura.frc.utn.edu.ar>
2026-03-03 22:58:05 -0300

    feat: add UserMapper

---
+++ Backend/src/Application/Extensions/UserMappingExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Application.Common.Users;
+using Domain.Entities;
+
+namespace Application.Extensions
+{
+    /// <summary>
+    /// Clase de extension para añadir métodos de mapeo
+    /// </summary>
+    public static class UserMappingExtensions
+    {
+        public static GetUserResponseDTO ToDto(this User user)
+        {
+            return new GetUserResponseDTO(
+                Id: user.Id,
+                Username: user.Username,
+                PictureId: user.PictureId
+            );
+        }
+    }
+}
