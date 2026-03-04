e1189909c144415c4d0d6f2585a5e6affedd6e12
Mateo Del Lungo <412337@tecnicatura.frc.utn.edu.ar>
2026-03-03 22:48:44 -0300

    feat: add UserController
    implement GetAll

---
+++ Backend/src/chat-in-realtime/Controllers/UserController.cs
@@ -0,0 +1,34 @@
+﻿using Application.Common;
+using Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace chat_in_realtime.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ApiController
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> GetAll()
+        {
+            var result = await _userService.GetAllUsersAsync();
+            return result.Match(
+                users => Ok(users),
+                errors => Problem(errors));
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
