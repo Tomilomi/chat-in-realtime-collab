6d0cccba2d56cfe4840907f4ded8edce2412d7b9
Mateo Del Lungo <412337@tecnicatura.frc.utn.edu.ar>
2026-03-09 22:30:21 -0300

    ApiController: add more statusCodes

--- Backend/src/chat-in-realtime/Controllers/ApiController.cs
+++ Backend/src/chat-in-realtime/Controllers/ApiController.cs
@@ -26,6 +26,8 @@ namespace chat_in_realtime.Controllers
                 ErrorType.NotFound => StatusCodes.Status404NotFound,
                 ErrorType.Conflict => StatusCodes.Status409Conflict,
                 ErrorType.Validation => StatusCodes.Status400BadRequest,
+                ErrorType.Unauthorized => StatusCodes.Status401Unauthorized,
+                ErrorType.Forbidden => StatusCodes.Status403Forbidden,
                 _ => StatusCodes.Status500InternalServerError
             };
 
