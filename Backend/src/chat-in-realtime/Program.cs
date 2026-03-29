09946964632322b0f4a788b802078d7c22ca430e
Mateo Del Lungo <412337@tecnicatura.frc.utn.edu.ar>
2026-03-29 00:12:46 -0300

    feat(seeder): refactor in program

--- Backend/src/chat-in-realtime/Program.cs
+++ Backend/src/chat-in-realtime/Program.cs
@@ -1,10 +1,7 @@
-using Infrastructure.Data;
 using chat_in_realtime.Hubs;
-using Domain.Entities;
 using Application.Extensions;
 using Infrastructure.Extensions;
 using chat_in_realtime.Extensions;
-using Domain.Enums;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -39,36 +36,7 @@ app.MapControllers();
 app.MapHub<ChatHub>("/chathub");
 
 // Reiniciar la base de datos al levantar
-using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.EnsureDeleted();
-    db.Database.EnsureCreated();
-
-    // SEEDER PARA TESTEAR
-
-    var picture1 = new Picture("/avatars/avatar1.png");
-    var picture2 = new Picture("/avatars/avatar2.png");
-    var picture3 = new Picture("/avatars/avatar3.png");
-    db.Pictures.Add(picture3);
-
-    var user1 = User.Create("Marcos", BCrypt.Net.BCrypt.HashPassword("password123"), picture1).Value;
-    user1.ChangeRole(UserRole.Admin);
-    var user2 = User.Create("Laura", BCrypt.Net.BCrypt.HashPassword("password123"), picture2).Value;
-
-    db.Users.AddRange(user1, user2);  // las Pictures se guardan solas
-
-    var messages = new List<Message>();
-    for (int i = 1; i <= 50; i++)
-    {
-        var sender = i % 2 == 0 ? user1 : user2;
-        var msg = new Message(sender, $"Mensaje de prueba número {i}");
-        msg.ForceTimestamp(DateTime.UtcNow.AddMinutes(-50 + i));
-        messages.Add(msg);
-    }
-
-    db.Messages.AddRange(messages);
-    db.SaveChanges();
-}
+// seeder
+Seeder.Initialize(app);
 
 app.Run();
